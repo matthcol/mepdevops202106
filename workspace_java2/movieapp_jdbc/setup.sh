2e3866dc4c66220bc9d7d283105112f8e42065a2
matthcol <matthias.colin@gmail.com>
2021-06-03 02:26:42 -0400

    fix script privileges + conf name + doc

--- workspace_java2/movieapp_jdbc/setup.sh
+++ workspace_java2/movieapp_jdbc/setup.sh
@@ -17,8 +17,9 @@ then
 	exit -1
 fi
 
-CONF_FILE=${CONF_DIR}/${PROPERTIES_BASEFILE}_$1
+CONF_FILE=${CONF_DIR}/${PROPERTIES_BASEFILE}-$1
 if [ ! -f ${CONF_FILE} ]
+then
 	echo "Configuration file does not exist: $CONF_FILE"
 	print_usage
 	exit -1
@@ -26,3 +27,4 @@ fi
 
 DEST_FILE=${RESOURCES_DIR}/${PROPERTIES_BASEFILE}
 cp ${CONF_FILE} ${DEST_FILE}
+echo "Project fixed: ${CONF_FILE} -> ${DEST_FILE}" 
