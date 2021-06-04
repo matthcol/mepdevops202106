30ac268858789161f3aef903374ed73e8822ea77
matthcol <matthias.colin@gmail.com>
2021-06-04 10:28:58 -0400

    docker samples

---
+++ docker/mysql-movie/sql/my_cine_initdb.sql
@@ -0,0 +1,13 @@
+-- connect with user root on none (database) 
+-- mysql -u root (xampp)
+-- sudo mysql -u root (linux without password)
+-- mysql -u root -p (linux with password)
+
+-- create database
+create database dbmovie CHARACTER SET utf8;
+
+-- create user with all privileges to database
+create user 'movie'@'*' identified by 'password';
+grant all privileges on dbmovie.* to 'movie'@'*';
+flush privileges;
+
